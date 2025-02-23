ff4d1cc77bb02d7e6f1d9da04c2234ac9db88330
Cristopher Olivares <cristopherolivares90@outlook.com>
2025-02-22 21:40:38 -0600

    Modificacion en mock.repository.ts para poder modificar el puerto de mysql y por defecto usar el 3306

--- src/infrastructure/repositories/mock.repository.ts
+++ src/infrastructure/repositories/mock.repository.ts
@@ -31,6 +31,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT || "3306") // Puerto de DB por defecto el 3306
 };
 
 class MySQLRepository implements LeadRepository {
